5eb514143a7cefbbf1f2422549b7c26b3e64e012
btaub <beau.taub@gmail.com>
2013-08-24 22:27:49 -0400

    Create createDropAllTables.py
    small script to convert show tables output to drop table.

---
+++ createDropAllTables.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+'''
+ToDo: extend script to connect to MySQL and
+      run show tables?
+'''
+
+dbname  = 'testDB'
+infile  = '/tmp/testDB_tables.txt'
+
+''' Sample input file - show tables;
+| adobe_payloads                                       |
+| app_lock_assignments                                 |
+| application_usage_logs                               |
+...etc
+'''
+
+outfile = '/tmp/' + dbname + '_DropTables.txt'
+
+''' Sample output file
+DROP TABLE IF EXISTS adobe_payloads;
+DROP TABLE IF EXISTS app_lock_assignments;
+DROP TABLE IF EXISTS application_usage_logs;
+'''
+
+readInFile     =  open(infile, 'r')
+writeOutFile   =  open(outfile, 'w')
+sqlcmd         = 'DROP TABLE IF EXISTS '
+
+writeOutFile.write('use ' + dbname + ';\n')
+
+for line in readInFile:
+    writeOutFile.write(sqlcmd + line.split('|')[1].strip() + ';\n')
+
+readInFile.close()
+readInFile.close()

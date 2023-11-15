0c31566375117b57ee67033073ddfff3ca7f861c
btaub <beau.taub@gmail.com>
2023-11-14 21:34:04 -0500

    clean up getBindVer.sh

--- getBindVer.sh
+++ getBindVer.sh
@@ -3,10 +3,10 @@
 if [[ -z $1 ]];then echo
     echo "Need domain name"
     exit
-else 
+else
     DOMAIN=$1
 fi
 
 for NS in $(dig -tns $DOMAIN +short); do
-	echo -e "Result for NS:\n $NS: $(dig +short @$NS version.bind txt ch)" ; sleep 1
+	echo -e "$NS: $(dig +short @$NS version.bind txt ch)"
 done

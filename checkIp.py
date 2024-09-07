ca438c03ecf74400ea5d785b95be5bb0f67c6cf7
btaub <beau.taub@gmail.com>
2024-09-07 00:50:08 -0400

    checkIp fixup + Tor check

--- checkIp.py
+++ checkIp.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
 
 import requests
-import sys
 import json
+import argparse
 
-''' TODO:
-    1) Add logging option
-    2) Change sys to argparse
-'''
+parser = argparse.ArgumentParser(description="Report IP findings from OTX, Tor, Shodan and IPinfo",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("ip")
+parser.add_argument("-v","--verbose",action="store_true",help="Verbose output",default=False)
+args = parser.parse_args()
+
+TOR = False
 
 sources = {
            "otx":"https://otx.alienvault.com/otxapi/indicators/ip/general/",
+           "tor":"https://onionoo.torproject.org/details?search=",
            "shodan":"https://internetdb.shodan.io/",
            "ipinfo":"https://ipinfo.io/"
           }
 
 for k,v in sources.items():
-    r = requests.get("%s%s" % (v , sys.argv[1]))
-    print("\n[+] Checking %s..." % k)
-    print("\n" + "+="*40 + "\n")
-    print(json.dumps(r.json(),indent=4))
+    r = requests.get(f"{v}{args.ip}")
+    resp = json.dumps(r.json(),indent=4)
+
+    if k == "tor":
+        if "nickname" in r.text:
+            TOR = True
+        else:
+            resp = "[x] Not a Tor relay"
+
+    print(f"\n[+] Checking {k}...")
+    print(f"\n" + "+="*40 + "\n")
+
+    if TOR:
+        print(f"\n[✔] {args.ip} is a Tor relay\n")
+        if args.verbose:
+            print(resp)
+        TOR = False
+    else:
+        print(resp)
+
     print("\n" + "+="*40)

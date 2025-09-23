c7c1544e24d15d7544d53f8ec43c84cd9989cc85
Beau Taub <1743650+btaub@users.noreply.github.com>
2025-09-23 18:05:12 -0400

    Update unshorten-link.py

--- unshorten-link.py
+++ unshorten-link.py
@@ -28,7 +28,7 @@ def share_google(url):
     while True:
         try:
             if not "share.google" in url:
-                print(url)
+                return(url)
                 break
             r = requests.get(url, allow_redirects=False)
 
@@ -42,22 +42,29 @@ def share_google(url):
 def linked_in(url):
     r = requests.get(url)
     for ln in r.text.split('\n'):
-        #if 'artdeco-button' in ln:
         if 'extern' in ln:
             for ln in ln.split('"'):
                 if ln.startswith('http'):
-                    print(ln)
+                    return(ln)
 
 # Generic location header-based HEAD request
 def head_req(url):
     r = requests.head(url)
-    print(r.headers['location'])
+    try:
+        res = r.headers['location']
+    except:
+        res = None
 
+    return(res)
 
 if __name__ == "__main__":
     if 'share.google' in url:
-        share_google(url)
+        res = share_google(url)
     if 'lnkd.in' in url:
-        linked_in(url)
+        res = head_req(url)
+        if not res:
+            res = linked_in(url)
     if 'bit.ly' in url or 'tinyurl.com' in url:
-        head_req(url)
+        res = head_req(url)
+        
+    print(res)
